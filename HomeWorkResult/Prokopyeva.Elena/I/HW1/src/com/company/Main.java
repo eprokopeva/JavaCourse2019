67880771d6fb292d33f3562ed5c85bea1aa8185f
eprokopeva <32647100+eprokopeva@users.noreply.github.com>
2019-09-18 12:07:18 +0600

    Add files via upload

---
+++ HomeWorkResult/Prokopyeva.Elena/I/HW1/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// определяем рандомное число
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(50);
+        int interval = 5;
+        int leftInterval = randomNumber-interval;
+        int rightInterval = randomNumber+interval;
+
+        System.out.println("Загаданное число " + randomNumber);
+
+        Scanner myObj = new Scanner(System.in);
+        boolean flag = true; //переменная для того, чтобы понять, когда остановить цикл
+
+        while (flag) {
+            System.out.println("Введите число от 0 до 50");
+            Integer number = myObj.nextInt();
+
+            if (number == randomNumber) {
+                System.out.println("Успех");
+                flag = false;
+            }
+             if (flag != false) {
+                 if (number > rightInterval || number < leftInterval) {
+                     System.out.println("Холодно");
+                 } else {
+                     System.out.println("Горячо");
+                 }
+             }
+
+        }
+
+    }
+}
